b0e6564cc9b87e82f38cc28ac22c044634c21a41
Kellymugambi <kellymgambi@.com>
2022-12-13 01:09:06 +0300

    added images, css styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     getfilmls()
-    //getfilml1()
+    getfilml1()
 })
 
 
@@ -10,27 +10,41 @@ function getfilmls (){
     fetch("http://localhost:3000/films/") 
     .then(response => response.json())
     .then(data => data.map((film) =>{
-        const li = document.createElement("li");
-        li.textContent = film.title
-        document.querySelector("#films").append(li)
+        const p = document.createElement("p")
+        p.className = "list"
+        p.textContent = film.title
+        document.querySelector("#films").append(p)
 
     }))
     
  }
+
     
     let runtime = document.querySelector("#runtime");
     let showtime = document.querySelector("#showtime");
-    let avtickets = document.querySelector("t#ickets");
+    let avtickets = document.querySelector("#tickets");
 
-// const getfilml1 =()=> {
-//     fetch("http://localhost:3000/films/1") 
-//     .then(response => response.json())
-//      .then((film1) => {
-//         const div2= document.querySelector("#content")
-//         const image = document.querySelector("#img")
-//         image.setAttribute(src,film1.poster)
-//         //div2.appendChild(image)
+const getfilml1 =()=> {
+    fetch("http://localhost:3000/films/1") 
+    .then(response => response.json())
+     .then((film1) => {
+        const image = document.querySelector("#img")
+        image.setAttribute("src", film1.poster)
         
+        
+        const h3 = document.querySelector("#h3")
+        const title =document.querySelector("#title")
+        title.textContent=film1.title
+        h3.appendChild(title)
 
-//     })
-//  }
+        runtime =film1.runtime
+        document.querySelector(".num").append(runtime,)
+
+        showtime=film1.showtime
+        document.querySelector(".show").append(showtime)
+        
+        // avtickets=(film1.capacity-film1.sold_tickets)
+        // document.querySelector(".tick").append(avtickets)
+        
+    })
+ }
